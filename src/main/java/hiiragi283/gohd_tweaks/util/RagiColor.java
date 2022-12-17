8cbedb779b19f6316c90341ed6c23b7df1ae66c0
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2022-12-17 18:27:28 +0900

    Add color tweaking class

---
+++ src/main/java/hiiragi283/gohd_tweaks/util/RagiColor.java
@@ -0,0 +1,26 @@
+package hiiragi283.gohd_tweaks.util;
+
+import java.awt.*;
+
+public class RagiColor {
+
+    //2つの色の中間色を取得するメソッド
+    public Color mixColor(Color color1, Color color2) {
+        //color1とcolor2の各RGB値の平均値を計算
+        int aveR = (color1.getRed() + color2.getRed()) / 2;
+        int aveG = (color1.getGreen() + color2.getGreen()) / 2;
+        int aveB = (color1.getBlue() + color2.getBlue()) / 2;
+        //中間 (管理) 色を返す
+        return new Color(aveR, aveG, aveB);
+    }
+
+    //混合色から元の色を取得するメソッド
+    public Color getBaseColor(Color colorMixed, Color colorBase1) {
+        //colorMixedとcolorBase1の各RGB値の差分を計算
+        int red2 = colorMixed.getRed() * 2 - colorBase1.getRed();
+        int green2 = colorMixed.getGreen() * 2 - colorBase1.getGreen();
+        int blue2 = colorMixed.getBlue() * 2 - colorBase1.getBlue();
+        //混成前の色を返す
+        return new Color(red2, green2, blue2);
+    }
+}
