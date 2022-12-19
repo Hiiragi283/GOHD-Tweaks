be407eb908b49e73bcfa0e59adb57051f827ed28
Hiiragi Russell Tsubasa <97942736+Hiiragi283@users.noreply.github.com>
2022-12-19 15:29:59 +0900

    Add new method for RagiNBT

--- src/main/java/hiiragi283/gohd_tweaks/util/RagiNBT.java
+++ src/main/java/hiiragi283/gohd_tweaks/util/RagiNBT.java
@@ -1,4 +1,29 @@
 package hiiragi283.gohd_tweaks.util;
 
+import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
+
+import javax.annotation.Nonnull;
+
 public class RagiNBT {
+
+    //stackに名前を設定するメソッド
+    public static ItemStack setName(@Nonnull ItemStack stack, String name) {
+        NBTTagCompound nbtBase = new NBTTagCompound();
+        NBTTagCompound nbtName = new NBTTagCompound();
+        nbtName.setString("Name", name);
+        nbtBase.setTag("display", nbtName);
+        stack.setTagCompound(nbtBase);
+        return stack;
+    }
+
+    //stackに翻訳した名前を設定するメソッド
+    public static ItemStack setLocName(@Nonnull ItemStack stack, String name) {
+        NBTTagCompound nbtBase = new NBTTagCompound();
+        NBTTagCompound nbtName = new NBTTagCompound();
+        nbtName.setString("LocName", name);
+        nbtBase.setTag("display", nbtName);
+        stack.setTagCompound(nbtBase);
+        return stack;
+    }
 }
